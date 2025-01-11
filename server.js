f355f8690092132b60ab99db1c527fe3e3e80602
Darshan <darshan310268@gmail.com>
2025-01-11 10:53:41 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+
+    socket.on("gesture", (gesture) => {
+      socket.broadcast.to(roomId).emit("gesture-received", gesture);
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
